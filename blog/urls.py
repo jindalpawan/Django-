55b24bd647b35e38d3345fc942975fbce777df2a
jindalpawan <jindalpawan99@gmail.com>
2020-05-15 20:10:05 +0530

    perma page update

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name="blog"
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
-    path('newpost', views.NewPost.as_view(), name='newpost'),
+    path('<int:pk>/', views.Perma.as_view(), name='onepost'),
+    path('newpost/', views.NewPost.as_view(), name='newpost'),
     #path('', views.perma, name='perma'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -10,13 +10,13 @@ class HomePage(TemplateView):
 		posts= Post.objects.all()
 		return render(request, 'blog/home.html', {'alldata':posts})
 
-'''
+
 class Perma(TemplateView):
 	def get(self,request, pk):
 		obj= get_object_or_404(Post, pk=pk)
-		return render(request, 'blog/')
+		return render(request, "blog/perma.html",{'post':obj})
 		
-'''
+
 class NewPost(TemplateView):
 	def get(self,request):
 		bg=NewpostForm(request.GET)
@@ -25,8 +25,11 @@ class NewPost(TemplateView):
 	def post(self, request):
 		bg=NewpostForm(request.POST)
 		if bg.is_valid():
-			bg.save()
-			return redirect('/blog')
+			title= bg.cleaned_data['title']
+			content=bg.cleaned_data['content']
+			p=Post(title= title, content=content)
+			p.save()
+			return redirect(reverse('blog:onepost',args=(p.pk,)))
 
 		else:
 			title= bg.cleaned_data['title']
