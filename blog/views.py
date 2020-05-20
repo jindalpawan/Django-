439f182770d674b57a660f5922dde157b497598b
jindalpawan <jindalpawan99@gmail.com>
2020-05-21 03:57:10 +0530

    all done

--- blog/views.py
+++ blog/views.py
@@ -10,13 +10,11 @@ import urllib3
 import json
 import random
 import hashlib
-import hmac
 
 str="asdfghjklpoiuytrewqzxcvbnm"
 
 def make_salt(length = 5):
     return ''.join(random.choice(str) for x in range(length))
-
 
 def make_pw_hash(name, pw, salt = None):
 	if not salt:
@@ -25,117 +23,8 @@ def make_pw_hash(name, pw, salt = None):
 	return '%s,%s' % (salt, h)
 
 def valid_pw(name, password, h):
-    return h == make_pw_hash(name, password, salt)
-
-	
-class Signup(TemplateView):
-	def get(self,request):
-		sig=SignupForm(request.GET)
-		response=render(request,"blog/signup.html",)
-		response.set_cookie('user_id', "")
-		return response
-
-	def post(self, request):
-		sig=SignupForm(request.POST)
-		if sig.is_valid():
-			username= sig.cleaned_data['username']
-			name=sig.cleaned_data['name']
-			email=sig.cleaned_data['email']
-			password=sig.cleaned_data['password']
-			vpass=sig.cleaned_data['vpass']
-			dic={}
-			if password != vpass:
-				dic['error_vpass']="Password not matched"
-			if (User.objects.filter(username=username).first()):
-				dic['error_username']="This username already exist"
-			if (User.objects.filter(email=email).first()):
-				dic['error_email']="This email already exist"
-
-			if dic:
-				dic['username']=username
-				dic['name']=name
-				dic['email']=email
-				return render(request,'blog/signup.html',dic)
-			else:
-				password=make_pw_hash(username, password)
-				p=User(username=username, name=name, email= email, password=password)
-				p.save()
-				response=redirect(reverse('blog:home',))
-				response.set_cookie('user_id',p.id)
-				return response
-		else:
-			username= sig.cleaned_data['username']
-			name=sig.cleaned_data['name']
-			email=sig.cleaned_data['email']
-			password=sig.cleaned_data['password']
-			vpass=sig.cleaned_data['vpass']
-			dic={'username': username, 'name':name, 'email':email}
-			return render(request,'blog/signup.html',dic)	
-
-
-
-class Login(TemplateView):
-	def get(self, request):
-		log=LoginForm(request.GET)
-		return render(request,"blog/login.html",)
-
-	def post(self, request):
-		log=LoginForm(request.POST)
-		if log.is_valid():
-			username= log.cleaned_data['username']
-			password=log.cleaned_data['password']
-			obj=User.objects.filter(username=username).first()
-			if obj and valid_pw(username, password, obj.password):
-				response=redirect(reverse('blog:home',))
-				response.set_cookie('user_id',obj.id)
-				return response				
-			else:
-				dic={'username':username, 'password':password,'error_password':"Username or Password not matcheds"}
-				return render(request,"blog/login.html",dic)
-
-
-class EditProfile(TemplateView):
-	def get(self,request):
-		sig=EditProfileForm(request.GET)
-		userid=request.COOKIES.get('user_id',0)
-		user=""
-		dic={}
-		if userid:
-			user=User.objects.filter(id=userid).first()
-			dic['username']=user.username
-			dic['email']=user.email
-			dic['name']=user.name
-		return render(request,"blog/edit_profile.html",dic)
-
-	def post(self, request):
-		sig=EditProfileForm(request.POST)
-		if sig.is_valid():
-			username=sig.cleaned_data['username']
-			name=sig.cleaned_data['name']
-			email=sig.cleaned_data['email']
-			p=User.objects.filter(username=username).first()
-			p.name= name
-			p.email=email
-			p.save()
-			return redirect(reverse('blog:home',))
-		
-		else:
-			username=sig.cleaned_data['username']
-			name=sig.cleaned_data['name']
-			email=sig.cleaned_data['email']
-			dic={'username': username, 'name':name, 'email':email}
-			return render(request,'blog/edit_profile.html',dic)	
-
-
-class Profile(TemplateView):
-	def get(self,request):
-		userid=request.COOKIES.get('user_id',0)
-		user=""
-		if userid:
-			posts=Post.objects.filter(user=userid)
-			user=User.objects.filter(id=userid).first()
-		return render(request, "blog/profile.html",{'user':user,'posts':posts})
-
+	salt = h.split(',')[0]
+	return h == make_pw_hash(name, password, salt)
 
 class HomePage(TemplateView):
 	def get(self, request):
@@ -186,21 +75,64 @@ class NewPost(TemplateView):
 			return render(request, 'blog/newpost.html',{'title': title, 'content':content})
 
 
+class Profile(TemplateView):
+	def get(self,request):
+		userid=request.COOKIES.get('user_id',0)
+		user=""
+		if userid:
+			posts=Post.objects.filter(user=userid)
+			user=User.objects.filter(id=userid).first()
+		return render(request, "blog/profile.html",{'user':user,'posts':posts})
+
+
+class EditProfile(TemplateView):
+	def get(self,request):
+		sig=EditProfileForm(request.GET)
+		userid=request.COOKIES.get('user_id',0)
+		user=""
+		dic={}
+		if userid:
+			user=User.objects.filter(id=userid).first()
+			dic['username']=user.username
+			dic['email']=user.email
+			dic['name']=user.name
+		return render(request,"blog/edit_profile.html",dic)
+
+	def post(self, request):
+		sig=EditProfileForm(request.POST)
+		if sig.is_valid():
+			username=sig.cleaned_data['username']
+			name=sig.cleaned_data['name']
+			email=sig.cleaned_data['email']
+			p=User.objects.filter(username=username).first()
+			p.name= name
+			p.email=email
+			p.save()
+			return redirect(reverse('blog:home',))
+		
+		else:
+			username=sig.cleaned_data['username']
+			name=sig.cleaned_data['name']
+			email=sig.cleaned_data['email']
+			dic={'username': username, 'name':name, 'email':email}
+			return render(request,'blog/edit_profile.html',dic)	
+
+
 class FacebookData(TemplateView):
 	def get(self, request):
 		URL= 'https://graph.facebook.com/v6.0/oauth/access_token?client_id=&redirect_uri=http://localhost:8000/blog/dataa&client_secret=&code='
 		URL2="https://graph.facebook.com/me?access_token="
 		code=request.GET['code']
-		code=URL+str(code)
+		code=URL+(code)
 		lib = urllib3.PoolManager()
 		r = lib.request('GET', code)
 		data=json.loads(r.data.decode('utf-8'))
 		token= data["access_token"]
-		URL2= URL2+str(token)+"&fields=id,name"
+		URL2= URL2+(token)+"&fields=id,name"
 		rr= lib.request('GET', URL2)
 		data2= json.loads(rr.data.decode('utf-8'))
-		username= str(data2['id'])
-		name=str(data2['name'])
+		username= (data2['id'])
+		name=(data2['name'])
 		password=username
 		obj= User.objects.filter(username=username).first()
 		if obj:
@@ -213,3 +145,68 @@ class FacebookData(TemplateView):
 			response=redirect(reverse('blog:home',))
 			response.set_cookie('user_id',obj.id)
 			return response
+
+
+class Signup(TemplateView):
+	def get(self,request):
+		sig=SignupForm(request.GET)
+		response=render(request,"blog/signup.html",)
+		response.set_cookie('user_id','')
+		return response
+
+	def post(self, request):
+		sig=SignupForm(request.POST)
+		if sig.is_valid():
+			username= sig.cleaned_data['username']
+			name=sig.cleaned_data['name']
+			email=sig.cleaned_data['email']
+			password=sig.cleaned_data['password']
+			vpass=sig.cleaned_data['vpass']
+			dic={}
+			if password != vpass:
+				dic['error_vpass']="Password not matched"
+			if (User.objects.filter(username=username).first()):
+				dic['error_username']="This username already exist"
+			if (User.objects.filter(email=email).first()):
+				dic['error_email']="This email already exist"
+
+			if dic:
+				dic['username']=username
+				dic['name']=name
+				dic['email']=email
+				return render(request,'blog/signup.html',dic)
+			else:
+				password=make_pw_hash(username, password)
+				p=User(username=username, name=name, email= email, password=password)
+				p.save()
+				response=redirect(reverse('blog:home',))
+				response.set_cookie('user_id',p.id)
+				return response
+		else:
+			username= sig.cleaned_data['username']
+			name=sig.cleaned_data['name']
+			email=sig.cleaned_data['email']
+			password=sig.cleaned_data['password']
+			vpass=sig.cleaned_data['vpass']
+			dic={'username': username, 'name':name, 'email':email}
+			return render(request,'blog/signup.html',dic)	
+
+
+class Login(TemplateView):
+	def get(self, request):
+		log=LoginForm(request.GET)
+		return render(request,"blog/login.html",)
+
+	def post(self, request):
+		log=LoginForm(request.POST)
+		if log.is_valid():
+			username= log.cleaned_data['username']
+			password=log.cleaned_data['password']
+			obj=User.objects.filter(username=username).first()
+			if obj and valid_pw(username, password, obj.password):
+				response=redirect(reverse('blog:home',))
+				response.set_cookie('user_id',obj.id)
+				return response				
+			else:
+				dic={'username':username, 'password':password,'error_password':"Username or Password not matcheds"}
+				return render(request,"blog/login.html",dic)
